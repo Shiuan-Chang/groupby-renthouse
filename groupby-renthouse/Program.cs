6987cfe35b64d8631b1e14212ba33f98c3f6a21d
MSI\icewi <icewindflyhero@gmail.com>
2024-11-22 19:52:00 +0800

    3rd commit

--- groupby-renthouse/Program.cs
+++ groupby-renthouse/Program.cs
@@ -17,7 +17,7 @@ namespace groupby_renthouse
                 new RentalProperty { Location = "大安區", Type = "分租套房", Rent = 8000, Layout = 1, roomNums = 3,floor = 1, IsNew = false, NearMRT = true, PetAllowed = true, HasParking = false, HasElevator = true, HasBalcony = false, ShortTermRental = true },
                 new RentalProperty { Location = "信義區", Type = "獨立套房", Rent = 20000, Layout = 2, roomNums = 2,floor = 3,IsNew = true, NearMRT = false, PetAllowed = false, HasParking = true, HasElevator = false, HasBalcony = true, ShortTermRental = false },
                 new RentalProperty { Location = "信義區", Type = "雅房", Rent = 6000, Layout = 1, roomNums = 4,floor = 2,IsNew = false, NearMRT = false, PetAllowed = false, HasParking = false, HasElevator = false, HasBalcony = false, ShortTermRental = true },
-                new RentalProperty { Location = "內湖區", Type = "整層住家", Rent = 25000, Layout = 3, roomNums = 3, floor = 2,IsNew = true, NearMRT = true, PetAllowed = true, HasParking = true, HasElevator = true, HasBalcony = true, ShortTermRental = false },
+                new RentalProperty { Location = "中正區", Type = "整層住家", Rent = 25000, Layout = 3, roomNums = 3, floor = 2,IsNew = true, NearMRT = true, PetAllowed = true, HasParking = true, HasElevator = true, HasBalcony = true, ShortTermRental = false },
                 new RentalProperty { Location = "內湖區", Type = "分租套房", Rent = 9000, Layout = 1, roomNums = 2,floor = 4,IsNew = false, NearMRT = true, PetAllowed = false, HasParking = false, HasElevator = false, HasBalcony = false, ShortTermRental = false },
                 new RentalProperty { Location = "士林區", Type = "獨立套房", Rent = 12000, Layout = 2, roomNums = 4,floor = 2,IsNew = false, NearMRT = true, PetAllowed = true, HasParking = false, HasElevator = true, HasBalcony = true, ShortTermRental = false },
                 new RentalProperty { Location = "士林區", Type = "車位", Rent = 3000, Layout = 0, roomNums = 1,floor = 1, IsNew = false, NearMRT = false, PetAllowed = false, HasParking = true, HasElevator = false, HasBalcony = false, ShortTermRental = false },
@@ -240,46 +240,82 @@ namespace groupby_renthouse
             //題目 3
             //問題：統計各個「位置」中不同「類型」的房源平均房間數（Rooms）。只包含房租超過 10000 且位於 1 樓以上的資料。
 
-            var Datas13 = properties.Where(x=>x.Rent > 10000 && x.floor > 1).GroupBy(x=>new { x.Location })
-                .Select(locationGroup => new
-                {
-                    Location = locationGroup.Key,
-                    Averages = locationGroup
-                    .GroupBy(y => y.Type) 
-                    .Select(typeGroup => new
-                    {
-                        Type = typeGroup.Key,
-                        AverageRooms = typeGroup.Average(y => y.roomNums) 
-                })}).ToList();
+            //var Datas13 = properties.Where(x=>x.Rent > 10000 && x.floor > 1).GroupBy(x=>new { x.Location })
+            //    .Select(locationGroup => new
+            //    {
+            //        Location = locationGroup.Key,
+            //        Averages = locationGroup
+            //        .GroupBy(y => y.Type) 
+            //        .Select(typeGroup => new
+            //        {
+            //            Type = typeGroup.Key,
+            //            AverageRooms = typeGroup.Average(y => y.roomNums) 
+            //    })}).ToList();
 
-            foreach (var data in Datas13)
-            {
-                Console.WriteLine($"Location: {data.Location}");
+            //foreach (var data in Datas13)
+            //{
+            //    Console.WriteLine($"Location: {data.Location}");
 
-                foreach (var avg in data.Averages)
-                {
-                    Console.WriteLine($"  Type: {avg.Type}, Average Rooms: {avg.AverageRooms}");
-                }
-            }
+            //    foreach (var avg in data.Averages)
+            //    {
+            //        Console.WriteLine($"  Type: {avg.Type}, Average Rooms: {avg.AverageRooms}");
+            //    }
+            //}
 
 
             //題目 4
             //問題：找出所有不允許養寵物且有陽台的房屋，並根據「房間數」和「樓層數」分組，計算每組的最高租金。
 
+            //var Datas14 = properties.Where(x=>x.PetAllowed == false && x.HasBalcony).GroupBy(x=>new {x.roomNums, x.Layout}).Select(x=>new
+            //{
+            //    group = x.Key,
+            //    MaxRent = x.Max(p=>p.Rent)
+            //}).ToList();
 
+            //foreach (var data in Datas14)
+            //{
+            //    Console.WriteLine($"房間數: {data.group.roomNums}, 樓層: {data.group.Layout}, 最高租金: {data.MaxRent}");
+            //}
 
             //題目 5
             //問題：找出每個「位置」中「樓層數」最高的「整層住家」類型，並顯示房屋的租金、房間數及是否有陽台。
 
+            //var Datas15 = properties.Where(x => x.Type == "整層住家").GroupBy(x => x.Location)
+            //    .Select(group => group.OrderByDescending(x => x.floor).FirstOrDefault())
+            //    .Select(x => new
+            //    {
+            //      Location = x.Location,
+            //      Rent = x.Rent,
+            //      Floor = x.floor,
+            //      RoomNums = x.roomNums, 
+            //      HasBalcony = x.HasBalcony 
+            //     }).ToList();
 
+            //foreach (var data in Datas15)
+            //{
+            //    Console.WriteLine($"位置: {data.Location}, 租金: {data.Rent},樓層: {data.Floor}, 房間數: {data.RoomNums}, 是否有陽台: {data.HasBalcony}");
+            //}
 
             //題目 6
             //問題: 找出每個位置地區中，不同的類型，能否養寵物他們的租金範圍都在多少~多少之間(列出min~max)
             //並且由房間數最高的依序排序 並顯示能否養寵物
 
+            var Datas16 = properties
+            .GroupBy(x => new { x.Location, x.Type }) 
+            .Select(group => new
+            {
+              Location = group.Key.Location,
+              Type = group.Key.Type,
+              CanHavePet = group.Any(x => x.PetAllowed), 
+              MinRent = group.Min(x => x.Rent), 
+              MaxRent = group.Max(x => x.Rent), 
+              MaxRoomNums = group.Max(x => x.roomNums) 
+            }).OrderByDescending(x => x.MaxRoomNums).ToList();
 
-
-
+            foreach (var data in Datas16)
+            {
+                Console.WriteLine($"位置: {data.Location}, 類型: {data.Type}, 可養寵物: {data.CanHavePet}, 租金範圍: {data.MinRent}~{data.MaxRent}, 房間數最高: {data.MaxRoomNums}");
+            }
             Console.ReadLine();
         }
     }
